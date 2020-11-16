788229ad9bb3cbd3886dafba544aae483212dc6c
Benjamin Tran <bektran@ucsc.edu>
2020-11-16 01:33:48 -0800

    Added in PlayerMovement script, EnemyFollowPlayerScript
    Also added in enemy cubes

---
+++ Assets/Scripts/NavMeshSurface.cs
@@ -0,0 +1,20 @@
+﻿//referenced Brackey's NavMesh video: https://www.youtube.com/watch?v=CHV1ymlw-P8
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavMeshSurface : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿//Use instead of Player script right to fix bugs
+//Referenced through Aaron Hibberd's video on player movement: https://www.youtube.com/watch?v=sXQI_0ILEW4
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public float moveSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	moveSpeed = 4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //checks to see if pressing any arrow keys
+        //if so will go horizontal if left or right
+        //will go vertical if up or down
+        	transform.Translate(moveSpeed*Input.GetAxis("Horizontal")*Time.deltaTime, 0f, moveSpeed*Input.GetAxis("Vertical")*Time.deltaTime);
+        
+    }
+}
