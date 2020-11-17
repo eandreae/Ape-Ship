8249c2b32f9d7a45e715dbe9a0fdb6d5476b89e0
Benjamin Tran <bektran@ucsc.edu>
2020-11-16 16:48:05 -0800

    Banana Pickups Script
    Added Coin object and Coin + PlayerScript scripts

---
+++ Assets/Scripts/CoinScript.cs
@@ -0,0 +1,28 @@
+﻿//Using BeepBoopIndie's video on collecting coins: https://www.youtube.com/watch?v=XnKKaL5iwDM
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(90 * Time.deltaTime, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other) {
+    	if(other.name == "Cube" || other.name == "Player") {
+    		other.GetComponent<PlayerScript>().points++;
+        	//Add 1 to points.
+        	Destroy(gameObject); //Destroys coin, when touched.
+        }
+    }
+}
