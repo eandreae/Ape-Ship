4a3977e72ba91928a419e2ec377f3031b897ad85
Derek Jean <derekjean99@gmail.com>
2021-01-22 19:46:32 -0800

    Tried to add player animation
    hello

---
+++ Assets/animationStateController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isWalkingHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+        isWalkingHash = Animator.StringToHash("isWalking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       bool isWalking = animator.GetBool(isWalkingHash);
+       bool forwardPressed = Input.GetKey("w");
+
+       if (!isWalking && forwardPressed)
+       {
+       	animator.SetBool(isWalkingHash, true);
+       }
+        
+       if(isWalking && !forwardPressed)
+       {
+       	animator.SetBool(isWalkingHash, false);
+       }
+    }
+}
