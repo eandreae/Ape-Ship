f4ffe67646a485437f610ca178522a1388db5c70
AJTyagi2024 <77591211+AJTyagi2024@users.noreply.github.com>
2021-01-27 14:08:35 -0800

    UIStuff
    -Main menu script
    -Temporary main menu theme
    -Added UI button press audio
    -Added transition panel and settings menu to main menu (the latter is nonfunctioning atm)

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public float startDelay = 1f;
+
+    public Animator startGameAnim;
+
+    public AudioSource buttonPress;
+
+    public GameObject settingsPanel;
+
+    public void StartGame()
+    {
+        Debug.Log("Start the game!");
+        //Play an animation that leads into the game using a UI panel
+        startGameAnim.Play("OutroAnim");
+        //This method will actually transition the game into the next scene
+        Invoke("finallyStart", startDelay);
+        //Plays a button press sound effect
+        buttonPress.Play();
+    }
+
+    void finallyStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Settings()
+    {
+        Debug.Log("Go to settings screen!");
+        //Sets the settings panel to be visible
+        settingsPanel.SetActive(true);
+        buttonPress.Play();
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit game!");
+        startGameAnim.Play("OutroAnim");
+        Invoke("finallyQuit", startDelay);
+        buttonPress.Play();
+    }
+
+    void finallyQuit()
+    {
+        Application.Quit();
+    }
+}
