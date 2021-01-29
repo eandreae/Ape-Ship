6c42a844ed4d0642cbe505c33c0bf28168a8d224
AJTyagi2024 <77591211+AJTyagi2024@users.noreply.github.com>
2021-01-28 17:40:54 -0800

    Minimap! Hold Q or M for it to pop up

---
+++ Assets/Scripts/Minimap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    public GameObject minimap;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If the player holds down either Q or M, the minimap will pop up so long as they are holding either of those keys down. 
+        //Q is quick to hit, while M is instictive for some players.
+        if (Input.GetKey("m") || Input.GetKey("q"))
+        {
+            minimap.SetActive(true);
+        }
+        else
+        {
+            minimap.SetActive(false);
+        }
+    }
+}
