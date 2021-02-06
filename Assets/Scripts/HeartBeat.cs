204c535ff9a6e7fcdf64341f9dd440f083813be2
JoeCressa <jcressa@ucsc.edu>
2021-02-05 20:22:26 -0800

    Added Heart Animation
    Added slow and fast heart animations

---
+++ Assets/Scripts/HeartBeat.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HeartBeat : MonoBehaviour
+{
+    private Animator animator;
+    private bool injured = false;
+    public Text heartColor;
+    GameObject heartObj;
+
+    void Start()
+    {
+       animator = GetComponent<Animator>();
+       heartObj = GameObject.FindGameObjectWithTag("Reactor");
+    }
+
+    void Update()
+    {
+        Debug.Log(injured);
+        if(heartColor.text == "red" && !injured)
+        {
+            animator.SetBool("Injured", true);
+            injured = true;
+        }
+        else if (heartColor.text == "green" || heartColor.text == "yellow" && injured)
+        {
+            animator.SetBool("Injured", false);
+            injured = false;
+        }
+    }
+}
