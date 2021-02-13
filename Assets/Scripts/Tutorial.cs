13148c485f45ffb206eefc73e1dd7c104200b52d
AJTyagi2024 <77591211+AJTyagi2024@users.noreply.github.com>
2021-02-13 13:55:57 -0800

    tutorial now its own individual script
    still not done yet tho

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tutorial : MonoBehaviour
+{
+    public AudioSource buttonPress;
+    public void ReturnToMenu()
+    {
+        buttonPress.Play();
+        gameObject.SetActive(false);
+    }
+}
