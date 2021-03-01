0f833675562f25e75485c8572498b35b0f08c4fe
JoeCressa <jcressa@ucsc.edu>
2021-02-28 21:02:38 -0800

    Set Up Lung Animations
    Lung animations in

---
+++ Assets/Scripts/LungAnim.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LungAnim : MonoBehaviour
+{
+    private Animator animator;
+    public Text lungColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lungColor.text == "yellow")
+        {
+            animator.speed = 1.5f;
+        }
+        else if (lungColor.text == "red")
+        {
+            animator.speed = 2.0f;
+        }
+        else if (lungColor.text == "green")
+        {
+            animator.speed = 1.0f;
+        }
+
+    }
+}
