59c514fe59aa06fea89572d0318a75115d60b0b8
AJTyagi2024 <77591211+AJTyagi2024@users.noreply.github.com>
2021-03-05 16:25:13 -0800

    added back minimap

---
+++ Assets/Scripts/Waypoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    public Transform[] waypoints;
+
+    Transform focus;
+
+    public void WhichWaypoint(int wp)
+    {
+        focus = waypoints[wp];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (focus != null){
+            transform.LookAt(focus);
+        }
+    }
+}
