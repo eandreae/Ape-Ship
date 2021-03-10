9b1592b546b000b3ebb80f6ba9b8ddefa1c697f5
jcressa <jcressa@ucsc.edu>
2021-03-09 18:14:50 -0800

    Heart Minigame Framework
    Heart minigame functional but not linked to game loop.

--- Assets/Scripts/ArrowScript.cs
+++ Assets/Scripts/ArrowScript.cs
@@ -9,55 +9,116 @@ public class ArrowScript : MonoBehaviour
     GameObject downArrow;
     GameObject rightArrow;
     GameObject leftArrow;
-    GameObject target;
+    GameObject temp;
+    static bool started = false;
+    static int rand;
+    static int count;
+    List<GameObject> arrowsList = new List<GameObject>();
+    List<GameObject> tileList = new List<GameObject>();
 
+    static readonly string[] arrowNames =  {
+        "Arrow_Left",
+        "Arrow_Right",
+        "Arrow_Down",
+        "Arrow_Up"
+    };
+    static readonly string[] tileNames =
+    {
+        "LEFT",
+        "RIGHT",
+        "DOWN",
+        "UP"
+    };
     // Start is called before the first frame update
     void Start()
     {
-        leftArrow = GameObject.Find("Arrow_Left");
-        leftArrow.SetActive(true);
-        rightArrow = GameObject.Find("Arrow_Right");
-        rightArrow.SetActive(true);
-        downArrow = GameObject.Find("Arrow_Down");
-        downArrow.SetActive(true);
-        upArrow = GameObject.Find("Arrow_Up");
-        upArrow.SetActive(true);
+        count = 0;
+
+        foreach(string name in arrowNames)
+        {
+            arrowsList.Add(GameObject.Find(name));
+            temp = GameObject.Find(name);
+            temp.SetActive(true);
+        }
+        foreach(string name in tileNames)
+        {
+            tileList.Add(GameObject.Find(name));
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //upArrow.SetActive(true);
+        /*if (started)
+        {
+            ChoosePattern();
+        }*/
+    }
+
+    private void ChoosePattern()
+    {
+        //randomly choose arrow for player to press
+        rand = Random.Range(0, 4);
+        //Debug.Log(rand);
+        arrowsList[rand].SetActive(true);
     }
 
     private void OnTriggerEnter(Collider coll)
     {
-        leftArrow.SetActive(false);
-        upArrow.SetActive(false);
-        rightArrow.SetActive(false);
-        downArrow.SetActive(false);
-
         if (coll.gameObject.name == "Player")
         {
-            //Debug.Log(gameObject.name);
-            if(gameObject.name == "LEFT")
+            if(gameObject.name == "Center" && started == false)
             {
-                //Debug.Log("YEEE");
-                leftArrow.SetActive(true);
+                foreach (GameObject obj in arrowsList)
+                {
+                    obj.SetActive(false);
+                }
+
+                started = true;
+                ChoosePattern();
+            }
+            //turn on all arrows if correct non-center tile is touched
+            if(started == true && tileList[rand].name == gameObject.name && gameObject.name != "Center")
+            {
+                arrowsList[rand].SetActive(false);
+                if(count == 5){
+                    foreach (GameObject obj in arrowsList)
+                    {
+                        obj.SetActive(true);
+                    }
+                } else
+                {
+                    ++count;
+                    ChoosePattern();
+                }
+                //turn off all arrows if incorrect non-center tile is touched
+            }
+            else if (started == true && tileList[rand] != gameObject && gameObject.name != "Center")
+            {
+                foreach (GameObject obj in arrowsList)
+                {
+                    obj.SetActive(false);
+                }
+
+            }
+
+            /*if (gameObject.name == "LEFT")
+            {
+                //arrowsList[0].SetActive(true);
             }
             else if (gameObject.name == "UP")
             {
-                upArrow.SetActive(true);
+                //arrowsList[3].SetActive(true);
             }
             else if (gameObject.name == "RIGHT")
             {
-                rightArrow.SetActive(true);
+                //arrowsList[1].SetActive(true);
             }
             else if (gameObject.name == "DOWN")
             {
-                downArrow.SetActive(true);
-            }
+                //arrowsList[2].SetActive(true);
+            }*/
 
         }
     }
@@ -68,19 +129,19 @@ public class ArrowScript : MonoBehaviour
         {
             if (gameObject.name == "LEFT")
             {
-                leftArrow.SetActive(false);
+                //arrowsList[0].SetActive(false);
             }
             else if (gameObject.name == "UP")
             {
-                upArrow.SetActive(false);
+                //arrowsList[3].SetActive(false);
             }
             else if (gameObject.name == "RIGHT")
             {
-                rightArrow.SetActive(false);
+                //arrowsList[1].SetActive(false);
             }
             else if (gameObject.name == "DOWN")
             {
-                downArrow.SetActive(false);
+                //arrowsList[2].SetActive(false);
             }
 
         }
