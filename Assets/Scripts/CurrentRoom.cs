db52dc652cae098b69990b1eea3da5c52c2e5775
AJTyagi2024 <77591211+AJTyagi2024@users.noreply.github.com>
2021-04-10 18:37:00 -0700

    Text showing what room you're in. Not done for hallways or that empty left room yet.

---
+++ Assets/Scripts/CurrentRoom.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CurrentRoom : MonoBehaviour
+{
+    public string roomName;
+
+    Text currentRoomText;
+    void Start()
+    {
+        currentRoomText = GameObject.Find("CurrentRoomText").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        currentRoomText.text = roomName;
+    }
+}
