4e74bed31b42388286faa2c17276b1406552f613
Benjamin Tran <bektran@ucsc.edu>
2021-04-19 03:23:15 -0700

    Finished Outlining (day 3)
    Finished the world outlining rendering. Renders everything in viewport.

--- Assets/Shaders/WorldOutlines/EdgeDetectionOutlinesInclude.hlsl
+++ Assets/Shaders/WorldOutlines/EdgeDetectionOutlinesInclude.hlsl
@@ -23,6 +23,10 @@
 #ifndef SOBELOUTLINES_INCLUDED
 #define SOBELOUTLINES_INCLUDED
 
+#include "DecodeDepthNormals.hlsl"
+
+TEXTURE2D(_DepthNormalsTexture); SAMPLER(sampler_DepthNormalsTexture);
+
 // The sobel effect runs by sampling the texture around a point to see
 // if there are any large changes. Each sample is multiplied by a convolution
 // matrix weight for the x and y components seperately. Each value is then
@@ -63,7 +67,6 @@ void DepthSobel_float(float2 UV, float Thickness, out float Out) {
     Out = length(sobel);
 }
 
-
 // This function runs the sobel algorithm over the opaque texture
 void ColorSobel_float(float2 UV, float Thickness, out float Out) {
     // We have to run the sobel algorithm over the RGB channels separately
@@ -88,6 +91,77 @@ void ColorSobel_float(float2 UV, float Thickness, out float Out) {
     // This is an alternate way to combine the three sobel values by taking the average
     // See which one you like better
     //Out = (length(sobelR) + length(sobelG) + length(sobelB)) / 3.0;
+}
+
+// Sample the depth normal map and decode depth and normal from the texture
+void GetDepthAndNormal(float2 uv, out float depth, out float3 normal) {
+    float4 coded = SAMPLE_TEXTURE2D(_DepthNormalsTexture, sampler_DepthNormalsTexture, uv);
+    DecodeDepthNormal(coded, depth, normal);
+}
+
+// A wrapper around the above function for use in a custom function node
+void CalculateDepthNormal_float(float2 UV, out float Depth, out float3 Normal) {
+    GetDepthAndNormal(UV, Depth, Normal);
+    // Normals are encoded from 0 to 1 in the texture. Remap them to -1 to 1 for easier use in the graph
+    Normal = Normal * 2 - 1;
+}
+
+// This function runs the sobel algorithm over the opaque texture
+void NormalsSobel_float(float2 UV, float Thickness, out float Out) {
+    // We have to run the sobel algorithm over the XYZ channels separately, like color
+    float2 sobelX = 0;
+    float2 sobelY = 0;
+    float2 sobelZ = 0;
+    // We can unroll this loop to make it more efficient
+    // The compiler is also smart enough to remove the i=4 iteration, which is always zero
+    [unroll] for (int i = 0; i < 9; i++) {
+        float depth;
+        float3 normal;
+        GetDepthAndNormal(UV + sobelSamplePoints[i] * Thickness, depth, normal);
+        // Create the kernel for this iteration
+        float2 kernel = float2(sobelXMatrix[i], sobelYMatrix[i]);
+        // Accumulate samples for each coordinate
+        sobelX += normal.x * kernel;
+        sobelY += normal.y * kernel;
+        sobelZ += normal.z * kernel;
+    }
+    // Get the final sobel value
+    // Combine the XYZ values by taking the one with the largest sobel value
+    Out = max(length(sobelX), max(length(sobelY), length(sobelZ)));
+}
+
+void DepthAndNormalsSobel_float(float2 UV, float Thickness, out float OutDepth, out float OutNormal) {
+    // This function calculates the normal and depth sobels at the same time
+    // using the depth encoded into the depth normals texture
+    float2 sobelX = 0;
+    float2 sobelY = 0;
+    float2 sobelZ = 0;
+    float2 sobelDepth = 0;
+    // We can unroll this loop to make it more efficient
+    // The compiler is also smart enough to remove the i=4 iteration, which is always zero
+    [unroll] for (int i = 0; i < 9; i++) {
+        float depth;
+        float3 normal;
+        GetDepthAndNormal(UV + sobelSamplePoints[i] * Thickness, depth, normal);
+        // Create the kernel for this iteration
+        float2 kernel = float2(sobelXMatrix[i], sobelYMatrix[i]);
+        // Accumulate samples for each channel
+        sobelX += normal.x * kernel;
+        sobelY += normal.y * kernel;
+        sobelZ += normal.z * kernel;
+        sobelDepth += depth * kernel;
+    }
+    // Get the final sobel values by taking the maximum
+    OutDepth = length(sobelDepth);
+    OutNormal = max(length(sobelX), max(length(sobelY), length(sobelZ)));
+}
+
+void ViewDirectionFromScreenUV_float(float2 In, out float3 Out) {
+    // Code by Keijiro Takahashi @_kzr and Ben Golus @bgolus
+    // Get the perspective projection
+    float2 p11_22 = float2(unity_CameraProjection._11, unity_CameraProjection._22);
+    // Convert the uvs into view space by "undoing" projection
+    Out = -normalize(float3((In * 2 - 1) / p11_22, -1));
 }
 
 #endif
