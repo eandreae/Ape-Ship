fdfd996ac9369de7dbcee67eb6213096d54adae9
Alec Z <az94587@gmail.com>
2021-04-22 00:27:46 -0700

    big camera
    added big camera

--- Assets/Scripts/NetworkManagerApeShip.cs
+++ Assets/Scripts/NetworkManagerApeShip.cs
@@ -17,8 +17,7 @@ public class NetworkManagerApeShip : NetworkManager
         GameObject player = Instantiate(playerPrefab, playerPrefab.GetComponent<Transform>());
         GameObject cam = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "PlayerCamera"));
         NetworkServer.AddPlayerForConnection(conn, player);
-        cam.GetComponent<PlayerCamera>().target = player;
-        NetworkServer.Spawn(cam);
+        //NetworkServer.Spawn(cam);
     }
 
     public override void OnServerDisconnect(NetworkConnection conn)
