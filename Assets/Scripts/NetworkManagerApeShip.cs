4fe80571e409bd08f77a5d677d9d6c1bb16d0d3d
Alec Z <az94587@gmail.com>
2021-04-22 00:16:59 -0700

    cam changes
    awefwfawfewafea

--- Assets/Scripts/NetworkManagerApeShip.cs
+++ Assets/Scripts/NetworkManagerApeShip.cs
@@ -15,7 +15,10 @@ public class NetworkManagerApeShip : NetworkManager
     public override void OnServerAddPlayer(NetworkConnection conn)
     {
         GameObject player = Instantiate(playerPrefab, playerPrefab.GetComponent<Transform>());
+        GameObject cam = Instantiate(spawnPrefabs.Find(prefab => prefab.name == "PlayerCamera"));
         NetworkServer.AddPlayerForConnection(conn, player);
+        cam.GetComponent<PlayerCamera>().target = player;
+        NetworkServer.Spawn(cam);
     }
 
     public override void OnServerDisconnect(NetworkConnection conn)
