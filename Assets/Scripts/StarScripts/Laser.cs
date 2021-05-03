f1452e617fd15e6d402a1ec1ee8f3d39202887d9
jcressa <jcressa@ucsc.edu>
2021-05-03 13:02:18 -0700

    Laser Object
    Basic laser object implemented with collisions

---
+++ Assets/Scripts/StarScripts/Laser.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private string hitting = "";
+    private bool reflecting = false;
+    private float scalar = 0.1f;
+    private Vector3 defaultPosition;
+    private Vector3 defaultScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultPosition = gameObject.transform.position;
+        defaultScale = gameObject.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if not currently hitting wall, move and scale until wall is hit
+        if (hitting == "Mirror")
+        {
+            reflecting = true;
+        }
+        else if (hitting == "Wall" && reflecting == false)
+        {
+            hitting = "Wall";
+        } else if(hitting == "")
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x - scalar, gameObject.transform.position.y, gameObject.transform.position.z);
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y + scalar, gameObject.transform.localScale.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Mirror")
+        {
+            hitting = "Mirror";
+        } else if(other.tag == "Walls" && reflecting == false)
+        {
+            hitting = "Wall";
+        }
+    }
+
+    private void Reflect()
+    {
+        GameObject temp = Instantiate(gameObject, gameObject.transform.position, gameObject.transform.rotation);
+        temp.transform.Rotate(45.0f, 0f, 0f);
+    }
+}
