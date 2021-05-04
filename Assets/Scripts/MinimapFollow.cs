879bd5cde808186e0f6ccef2c91f36e599642cce
TinkerGolemancer <47468248+TinkerGolemancer@users.noreply.github.com>
2021-05-03 21:38:15 -0700

    Revert "Revert "Merge branch 'Development' of https://github.com/eandreae/Ape-Ship into Development""
    This reverts commit 8cde8704769e3787e343b4439ccd84174390d10c.

---
+++ Assets/Scripts/MinimapFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapFollow : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    public Transform player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(player.position * 0.163f * Time.fixedDeltaTime);
+    }
+}
