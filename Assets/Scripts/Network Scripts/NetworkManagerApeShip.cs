cfac87a3d826c5bb9fce42544343b2bb9d11361c
Brad Kim <brckim@ucsc.edu>
2021-05-08 21:58:10 -0700

    Update NetworkManagerApeShip.cs

--- Assets/Scripts/Network Scripts/NetworkManagerApeShip.cs
+++ Assets/Scripts/Network Scripts/NetworkManagerApeShip.cs
@@ -72,17 +72,21 @@ public class NetworkManagerApeShip : NetworkRoomManager
 
     public override void OnClientSceneChanged(NetworkConnection conn)
     {
-        base.OnClientSceneChanged(conn); 
-        
-        //GameObject player = Instantiate(playerPrefab, playerPrefab.GetComponent<Transform>());
-        //player.GetComponent<Player>().playerNum = numPlayers;
-        //NetworkServer.ReplacePlayerForConnection(conn, player);   
+        base.OnClientSceneChanged(conn);
+
+        GameObject cube = conn.identity.gameObject;
+        GameObject player = Instantiate(playerPrefab, playerPrefab.GetComponent<Transform>());
+        player.GetComponent<Player>().playerNum = numPlayers;
+        NetworkServer.ReplacePlayerForConnection(conn, player);
+        NetworkServer.Destroy(cube);
     }
     
     
     public override void OnServerSceneChanged(string newSceneName)
     {
         //base.OnServerSceneChanged(newSceneName);
+        //array of gameobjects to store cubes
+
         /*
         Debug.Log("numplayers " + numPlayers);
         Debug.Log(newSceneName);
