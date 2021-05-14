040e08f1f2dee604e2aaecef696a80a78748bb8e
Brad Kim <brckim@ucsc.edu>
2021-05-13 22:51:41 -0700

    WIP ready button

---
+++ Assets/Scripts/Network Scripts/LobbyPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class LobbyPlayer : NetworkBehaviour
+{
+
+    [SerializeField] private NetworkRoomPlayer player = null;
+    bool currentvalue = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.GetComponent<NetworkRoomPlayer>();
+        Debug.Log(player);
+    }
+
+    public void toggle()
+    {
+        player = this.GetComponent<NetworkRoomPlayer>();
+        Debug.Log(player);
+        player.CmdChangeReadyState(!currentvalue);
+    }
+
+    public void setbool(bool value)
+    {
+        player = this.GetComponent<NetworkRoomPlayer>();
+        Debug.Log(player);
+        player.CmdChangeReadyState(value);
+        currentvalue = value;
+    }
+}
