3ce1a670ea0ea08b987e90c8e28d94139cb697f1
Brad Kim <brckim@ucsc.edu>
2021-05-13 23:19:43 -0700

    lobby QOL (spacebar to toggle ready; ready button; disconnect works)

--- Assets/Scripts/Network Scripts/LobbyPlayer.cs
+++ Assets/Scripts/Network Scripts/LobbyPlayer.cs
@@ -2,30 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using UnityEngine.UI;
 
 public class LobbyPlayer : NetworkBehaviour
 {
 
     [SerializeField] private NetworkRoomPlayer player = null;
+    [SerializeField] private Button readybutton = null;
+    [SerializeField] private Button disconnect = null;
     bool currentvalue = false;
 
     // Start is called before the first frame update
     void Start()
     {
         player = this.GetComponent<NetworkRoomPlayer>();
+        readybutton = GameObject.Find("ready").GetComponent<Button>();
+        readybutton.onClick.AddListener(toggle);
+        disconnect = GameObject.Find("disconnect").GetComponent<Button>();
+        disconnect.onClick.AddListener(() => setbool(true));
         Debug.Log(player);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            toggle();
+
     }
 
     public void toggle()
     {
-        player = this.GetComponent<NetworkRoomPlayer>();
         Debug.Log(player);
         player.CmdChangeReadyState(!currentvalue);
+        currentvalue = !currentvalue;
     }
 
     public void setbool(bool value)
     {
-        player = this.GetComponent<NetworkRoomPlayer>();
         Debug.Log(player);
         player.CmdChangeReadyState(value);
         currentvalue = value;
