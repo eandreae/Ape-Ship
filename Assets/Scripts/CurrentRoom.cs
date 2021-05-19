6f5abda37bbf12c82fc88653416118f7a1f33ffc
Alec Z <az94587@gmail.com>
2021-05-18 22:42:41 -0700

    Fixed camera bug for multiplayer

--- Assets/Scripts/CurrentRoom.cs
+++ Assets/Scripts/CurrentRoom.cs
@@ -2,21 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
+
 public class CurrentRoom : MonoBehaviour
 {
     public string roomName;
-
+    NetworkManager nm;
     Text currentRoomText;
+
     void Start()
     {
         currentRoomText = GameObject.Find("CurrentRoomText").GetComponent<Text>();
+        nm = GameObject.FindObjectOfType<NetworkManager>();
     }
 
     private void OnTriggerEnter(Collider other)
-    {
+    {   
         if (other.CompareTag("Player"))
         {
-            currentRoomText.text = roomName;
+            // if there IS a network manager, only change text if it's the Local Player.
+            if ( !nm || other.GetComponent<Player>().isLocalPlayer )
+                currentRoomText.text = roomName;
         }
     }
 }
