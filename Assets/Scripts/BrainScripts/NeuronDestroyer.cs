7b6bab39969f58538397dbf433c4dafbc7d53624
PracticalPotato <calvinrong@gmail.com>
2021-05-22 22:41:36 -0700

    3 Color Neurons
    Got it.
    Placeholder cylinders for dropoffs of other two colors.

---
+++ Assets/Scripts/BrainScripts/NeuronDestroyer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class NeuronDestroyer : MonoBehaviour
+{
+    public UnityEvent OnNeuronDeposit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.name == "neuronDestroyerBlue" && this.gameObject.name == "neuronSpawnBlue(Clone)")
+        {
+            Destruction();
+        }
+        
+        if (coll.gameObject.name == "neuronDestroyerRed" && this.gameObject.name == "neuronSpawnRed(Clone)")
+        {
+            Destruction();
+        }
+
+        if (coll.gameObject.name == "neuronDestroyerGreen" && this.gameObject.name == "neuronSpawnGreen(Clone)")
+        {
+            Destruction();
+        }
+    }
+    void Destruction()
+    {
+        Destroy(this.gameObject);
+        //fix the brain
+        OnNeuronDeposit.Invoke();
+    }
+}
--- Assets/Scripts/BrainScripts/spawner.cs
+++ Assets/Scripts/BrainScripts/spawner.cs
@@ -7,72 +7,39 @@ using UnityEngine.UI;
 public class spawner : MonoBehaviour
 {
     public Transform spawnPos;
-    public GameObject spawnee;
-    string currColor;
-    public Text nodeColor;
+    public GameObject neuronBlue;
+    public GameObject neuronRed;
+    public GameObject neuronGreen;
     Vector3 spawnLoc;
-    GameObject canister1;
-    GameObject canister2;
-    GameObject target;
 
     // Start is called before the first frame update
     void Start()
     {
-        currColor = nodeColor.text;
-        canister1 = GameObject.Find("Air_Tank_1");
-        canister2 = GameObject.Find("Air_Tank_2");
-        canister1.SetActive(true);
-        canister2.SetActive(true);
 
     }
 
     // Update is called once per frame
     void Update()
-    {/*
-        spawnLoc = new Vector3(spawnPos.position.x + Random.Range(0.0f, 5.0f), (float)spawnPos.position.y, spawnPos.position.z + Random.Range(0.0f, 5.0f));
-
-
-        if (nodeColor.text == "red")
-        {
-            canister1.SetActive(false);
-            canister2.SetActive(false);
-        }
-        else if (nodeColor.text == "yellow")
-        {
-            canister1.SetActive(true);
-            canister2.SetActive(false);
-        }
-        else if (nodeColor.text == "green")
-        {
-            canister1.SetActive(true);
-            canister2.SetActive(true);
-        }
-
-        if (nodeColor.text != currColor)
-        {
-            if (nodeColor.text != "green")
-            {
-                currColor = nodeColor.text;
-                GameObject temp = Instantiate(spawnee, spawnLoc, spawnPos.rotation);
-                temp.GetComponent<Rigidbody>().useGravity = true;
-            }
-            else if (nodeColor.text == "green")
-            {
-                currColor = nodeColor.text;
-                if(GameObject.Find("Air_Tank_3(Clone)") != null)
-                {
-                    target = GameObject.Find("Air_Tank_3(Clone)");
-                    Destroy(target);
-                }
-            }
-        }*/
+    {
 
     }
 
     public void Spawn() {
-        spawnLoc = new Vector3(spawnPos.position.x + Random.Range(0.0f,8.0f), (float)spawnPos.position.y, spawnPos.position.z + Random.Range(0.0f, 5.0f));
+        spawnLoc = new Vector3(spawnPos.position.x + Random.Range(-4.0f,4.0f), (float)spawnPos.position.y, spawnPos.position.z + Random.Range(-3.0f, 3.0f));
+        GameObject spawnee = neuronBlue;
+
+        int sw = Random.Range(0, 3);
+        switch(sw)
+        {
+            case 1:
+                spawnee = neuronRed;
+                break;
+            case 2:
+                spawnee = neuronGreen;
+                break;
+        }
+
         GameObject temp = Instantiate(spawnee, spawnLoc, spawnPos.rotation);
         temp.GetComponent<Rigidbody>().useGravity = true;
-        //temp.GetComponent<destroyer>().enabled = true;
     }
 }
