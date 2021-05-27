8b42acc5bd8fac91c216dd1579b681ea5312d81b
AJTyagi2024 <77591211+AJTyagi2024@users.noreply.github.com>
2021-05-26 19:48:52 -0700

    new UI manager script for transitions

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,20 +4,44 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    public Animator blackBarTop;
+    public Animator blackBarBottom;
     public Animator progressBar;
     public Animator minimap;
+    public Animator healthOxygen;
+    public Animator roomName;
+
+    public float introDuration;
+
     [Header("Endgame")]
     public Animator[] minimapNodes;
+
+    Animator destructTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Invoke("SlideInUI", introDuration);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SlideInUI()
     {
-        
+        blackBarTop.Play("TopBarLeave");
+        blackBarBottom.Play("BottomBarLeave");
+        progressBar.Play("ProgressBarSlideIn");
+        minimap.Play("MinimapSlideIn");
+        healthOxygen.Play("HealthOxygenSlideIn");
+        roomName.Play("RoomNameSlideIn");
     }
+
+    public void ReplaceProgressBar()
+    {
+        progressBar.Play("ProgressBarFadeOut");
+        destructTimer.Play("TimerFadeIn");
+        foreach (Animator minimapNode in minimapNodes)
+        {
+            minimapNode.Play("MinimapNodeFadeOut");
+        }
+    }
+
 }
