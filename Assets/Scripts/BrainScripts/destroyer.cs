7f7ddaff786766e8d4d5563de45a27104c3a67f6
Alec Zhang <az94587@gmail.com>
2021-05-27 01:24:00 -0700

    testing stomach/lungs/brain minigames in multiplayer

--- Assets/Scripts/BrainScripts/destroyer.cs
+++ Assets/Scripts/BrainScripts/destroyer.cs
@@ -4,34 +4,43 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
 using UnityEngine.Events;
+using Mirror;
 
 public class destroyer : MonoBehaviour
 {
+    public NetworkManager nm;
     public float lifetime = 5f;
     public Text nodeColor;
-    public UnityEvent OnNeuronDeposit;
+    public NodeInstanceManager selfNode;
+    //public UnityEvent OnNeuronDeposit;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(!nodeColor){
-            if(this.GetComponent<ItemScript>().type == "Neuron" || this.GetComponent<ItemScript>().type == "Canister2" ){
-                this.nodeColor = GameObject.Find("NavigationColor").GetComponent<Text>();
-            }
-            else if(this.GetComponent<ItemScript>().type == "Canister1" || this.GetComponent<ItemScript>().type == "Canister2" ){
-                this.nodeColor = GameObject.Find("OxygenColor").GetComponent<Text>();
-            }
-            else {
-                this.nodeColor = GameObject.Find("StomachColor").GetComponent<Text>();
-            }
-        }
+        nm = GameObject.FindObjectOfType<NetworkManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if (lifetime > 0)
+        if (nm){
+            if (this.GetComponent<ItemScript>().type == "Canister1"){
+                this.nodeColor = GameObject.Find("OxygenColor").GetComponent<Text>();
+                selfNode = GameObject.Find("LungTarget").GetComponent<NodeInstanceManager>();
+            }
+            else if (this.GetComponent<ItemScript>().type == "Canister2" ) {
+                this.nodeColor = GameObject.Find("Oxygen2Color").GetComponent<Text>();
+                selfNode = GameObject.Find("Lungs_2Target").GetComponent<NodeInstanceManager>();
+            }
+            else {
+                this.nodeColor = GameObject.Find("StomachColor").GetComponent<Text>();
+                selfNode = GameObject.Find("StomachTarget").GetComponent<NodeInstanceManager>();
+            }
+        }
+
+
+        if (lifetime > 0 && !(this.GetComponent<ItemScript>().type == "Canister1"
+                         || this.GetComponent<ItemScript>().type == "Canister2"))
         {
             lifetime -= Time.deltaTime;
             if (lifetime <= 0 && !this.gameObject.GetComponent<ItemScript>().pickedUp)
@@ -66,12 +75,14 @@ public class destroyer : MonoBehaviour
         {
             if (this.gameObject.name == "Air_Tank_3(Clone)")
             {
+                Debug.Log("air canister destroyed");
                 Destruction();
             }
         } else if (coll.gameObject.name == "canisterDestroyer(O1)")
         {
             if (this.gameObject.name == "Air_Tank_3(O1)(Clone)")
             {
+                Debug.Log("air canister destroyed");
                 Destruction();
             }
         }
@@ -82,6 +93,7 @@ public class destroyer : MonoBehaviour
     {
         Destroy(this.gameObject);
         //fix the brain
-        OnNeuronDeposit.Invoke();
+        //OnNeuronDeposit.Invoke();
+        selfNode.FixNode();
     }
 }
--- Assets/Scripts/BrainScripts/spawner.cs
+++ Assets/Scripts/BrainScripts/spawner.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using Mirror;
 
 public class spawner : MonoBehaviour
 {
+    public NetworkManager nm;
     public Transform spawnPos;
     public GameObject neuronBlue;
     public GameObject neuronRed;
@@ -16,7 +17,7 @@ public class spawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        nm = GameObject.FindObjectOfType<NetworkManager>();
     }
 
     // Update is called once per frame
@@ -27,40 +28,68 @@ public class spawner : MonoBehaviour
 
     public void Spawn() {
         spawnLoc = new Vector3(spawnPos.position.x + Random.Range(-4.0f,4.0f), (float)spawnPos.position.y, spawnPos.position.z + Random.Range(-3.0f, 3.0f));
-        GameObject spawnee = neuronBlue;
+        
+        GameObject spawnee;
+        if (nm)
+            spawnee = nm.spawnPrefabs[6];
+        else
+            spawnee = neuronBlue;
 
         int sw = Random.Range(0, 3);
         switch(sw)
         {
             case 1:
-                spawnee = neuronRed;
+                if (nm)
+                    spawnee = nm.spawnPrefabs[7];
+                else
+                    spawnee = neuronRed;
                 break;
             case 2:
-                spawnee = neuronGreen;
+                if (nm)
+                    spawnee = nm.spawnPrefabs[8];
+                else
+                    spawnee = neuronGreen;
                 break;
         }
 
         GameObject temp = Instantiate(spawnee, spawnLoc, spawnPos.rotation);
         temp.GetComponent<Rigidbody>().useGravity = true;
+        if (nm)
+                NetworkServer.Spawn(temp);
+
 
         if(brainColor.text == "red")
         {
             spawnLoc = new Vector3(spawnPos.position.x + Random.Range(-4.0f, 4.0f), (float)spawnPos.position.y, spawnPos.position.z + Random.Range(-3.0f, 3.0f));
-            GameObject spawnee2 = neuronBlue;
+            GameObject spawnee2;
+            if (nm)
+                spawnee2 = nm.spawnPrefabs[6];
+            else
+                spawnee2 = neuronBlue;
 
             int sw2 = Random.Range(0, 3);
             switch (sw2)
             {
                 case 1:
-                    spawnee = neuronRed;
+                    if (nm)
+                        spawnee2 = nm.spawnPrefabs[7];
+                    else
+                        spawnee2 = neuronRed;
                     break;
                 case 2:
-                    spawnee = neuronGreen;
+                   if (nm)
+                        spawnee2 = nm.spawnPrefabs[8];
+                    else
+                        spawnee2 = neuronGreen;
+
                     break;
             }
 
             GameObject temp2 = Instantiate(spawnee2, spawnLoc, spawnPos.rotation);
             temp2.GetComponent<Rigidbody>().useGravity = true;
+
+            if (nm)
+                NetworkServer.Spawn(temp);
 
         }
     }
