95500d11ec214f1f9a8a8595fc272b2f0945fd84
jcressa <jcressa@ucsc.edu>
2021-05-27 18:29:45 -0700

    Battery Ending
    Player now needs battery to achieve victory

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -85,6 +85,12 @@ public class ItemScript : MonoBehaviour
             else if (type == "Nuke"){
                 transform.rotation = playerRoot.rotation * Quaternion.Euler(0, 90, 0); // keep rotation at a constant value
             }
+            else if (type == "Battery")
+            {
+                transform.rotation = playerRoot.rotation * Quaternion.Euler(0, 90, 0); // keep rotation at a constant value
+                AlterSpeed(4f);
+            }
+
         }
     }
 
