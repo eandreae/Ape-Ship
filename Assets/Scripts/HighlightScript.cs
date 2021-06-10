f8195ff52a305d66c725eedbde66ae50a8922feb
PracticalPotato <calvinrong@gmail.com>
2021-06-09 20:24:18 -0700

    Highlight for button, spacebar to spawn food

---
+++ Assets/Scripts/HighlightScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightScript : MonoBehaviour
+{
+    public GameObject highlight;
+    // Start is called before the first frame update
+
+    public void highlightOn() {
+        highlight.SetActive(true);
+    }
+
+    public void highlightOff() {
+        highlight.SetActive(false);
+    }
+
+}
