eb9323c56fe987b92fd1638072ad02dbea5db182
Brad Kim <brckim@ucsc.edu>
2021-06-10 00:23:20 -0700

    Update LobbyPlayer.cs

--- Assets/Scripts/Network Scripts/LobbyPlayer.cs
+++ Assets/Scripts/Network Scripts/LobbyPlayer.cs
@@ -44,7 +44,12 @@ public class LobbyPlayer : NetworkBehaviour
 
         readyindicator.SetActive(player.readyToBegin);
 
-        if (SceneManager.GetActiveScene().name == "game") Destroy(this.gameObject);
+        if (SceneManager.GetActiveScene().name == "game") Invoke("killme", 3f)
+    }
+
+    private void killme()
+    {
+        Destroy(this.gameObject);
     }
 
 
