4d5b8efba88223fe02d2aed08735abdd72045764
jcressa <jcressa@ucsc.edu>
2021-06-10 15:00:18 -0700

    Fixed Mirrors

--- Assets/Scripts/StarScripts/ShootLaser.cs
+++ Assets/Scripts/StarScripts/ShootLaser.cs
@@ -18,36 +18,36 @@ public class ShootLaser : MonoBehaviour
     GameObject mirror2;
     GameObject mirror3;
     public List<float> angles = new List<float>();
+    //single player angles
     static float[] angleList =
     {
-        //multiplayer angles
-        /*158.0f,
-        -77.0f,
-        0.0f,
-        85.0f,
-        -35.0f,
-        -123.0f,
-        -186.5f,
-        0.0f,
-        157.0f*/
-        //single player angles
         0f,
         103f,
         0f,
         106f,
         0f,
-        -77f,
-        -2.2f,
+        -85f,
         0f,
-        -196.0f
+        0f,
+        160.0f
     };
+    //multiplayer angles
+    /*158.0f,
+    -77.0f,
+    0.0f,
+    85.0f,
+    -35.0f,
+    -123.0f,
+    -186.5f,
+    0.0f,
+    157.0f*/
 
 
     private void Start()
     {
         mirror1 = GameObject.Find("MirrorCart/Rail");
         mirror2 = GameObject.Find("MirrorCart/Rail (1)");
-        mirror3 = GameObject.Find("MirrorCart/Rail (2)");
+        mirror3 = GameObject.Find("MirrorCart/Rail (3)");
 
         //ChooseRotation(mirror1.transform.GetChild(0).gameObject, mirror2.transform.GetChild(0).gameObject, mirror3.transform.GetChild(0).gameObject);
     }
@@ -72,19 +72,19 @@ public class ShootLaser : MonoBehaviour
 
     public void ShuffleMirror()
     {
-        GameObject child1 = mirror1.transform.GetChild(1).gameObject;
-        GameObject child2 = mirror2.transform.GetChild(1).gameObject;
-        GameObject child3 = mirror3.transform.GetChild(1).gameObject;
+        GameObject stop1 = mirror1.transform.GetChild(2).gameObject;
+        GameObject stop2 = mirror2.transform.GetChild(2).gameObject;
+        GameObject stop3 = mirror3.transform.GetChild(2).gameObject;
 
-        float RandZ1 = Random.Range(child1.transform.GetChild(0).gameObject.transform.position.z, child1.transform.GetChild(1).gameObject.transform.position.z);
-        float RandZ2 = Random.Range(child2.transform.GetChild(0).gameObject.transform.position.z, child2.transform.GetChild(1).gameObject.transform.position.z);
-        float RandZ3 = Random.Range(child3.transform.GetChild(0).gameObject.transform.position.z, child3.transform.GetChild(1).gameObject.transform.position.z);
+        float RandZ1 = Random.Range(stop1.transform.GetChild(0).gameObject.transform.position.z, stop1.transform.GetChild(1).gameObject.transform.position.z);
+        float RandZ2 = Random.Range(stop2.transform.GetChild(0).gameObject.transform.position.z, stop2.transform.GetChild(1).gameObject.transform.position.z);
+        float RandZ3 = Random.Range(stop3.transform.GetChild(0).gameObject.transform.position.z, stop3.transform.GetChild(1).gameObject.transform.position.z);
 
-        mirror1.transform.GetChild(0).gameObject.transform.position = new Vector3(mirror1.transform.GetChild(0).gameObject.transform.position.x, mirror1.transform.GetChild(0).gameObject.transform.position.y, RandZ1);
-        mirror2.transform.GetChild(0).gameObject.transform.position = new Vector3(mirror2.transform.GetChild(0).gameObject.transform.position.x, mirror2.transform.GetChild(0).gameObject.transform.position.y, RandZ2);
-        mirror3.transform.GetChild(0).gameObject.transform.position = new Vector3(mirror3.transform.GetChild(0).gameObject.transform.position.x, mirror3.transform.GetChild(0).gameObject.transform.position.y, RandZ3);
+        mirror1.transform.GetChild(1).gameObject.transform.position = new Vector3(mirror1.transform.GetChild(1).gameObject.transform.position.x, mirror1.transform.GetChild(1).gameObject.transform.position.y, RandZ1);
+        mirror2.transform.GetChild(1).gameObject.transform.position = new Vector3(mirror2.transform.GetChild(1).gameObject.transform.position.x, mirror2.transform.GetChild(1).gameObject.transform.position.y, RandZ2);
+        mirror3.transform.GetChild(1).gameObject.transform.position = new Vector3(mirror3.transform.GetChild(1).gameObject.transform.position.x, mirror3.transform.GetChild(1).gameObject.transform.position.y, RandZ3);
 
-        ChooseRotation(mirror1.transform.GetChild(0).gameObject, mirror2.transform.GetChild(0).gameObject, mirror3.transform.GetChild(0).gameObject);
+        ChooseRotation(mirror1.transform.GetChild(1).gameObject, mirror2.transform.GetChild(1).gameObject, mirror3.transform.GetChild(1).gameObject);
     }
 
     private void ChooseRotation(GameObject cart1, GameObject cart2, GameObject cart3)
