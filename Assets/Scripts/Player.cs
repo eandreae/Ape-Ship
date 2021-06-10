68b02a94797aa82bbc70c455fffab8f671f34c51
Alec Z <az94587@gmail.com>
2021-06-09 21:42:16 -0700

    thank you brad, very cool!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -98,6 +98,7 @@ public class Player : NetworkBehaviour
             InvokeRepeating("PlayWalkingNoise", 0, 0.4f); // only play footsteps for localplayer
             gm.localp = this;
             FindObjectOfType<PlayerCamera>().target = this.gameObject;
+            this.anim.applyRootMotion = true;
         }
         else {
             Object.Destroy(this.wpArrow); // get rid of waypoints for non-local players
